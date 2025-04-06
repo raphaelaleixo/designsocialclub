8d5dd8f0435eee14fefb4a49166d331533172784
Raphael Aleixo Avellar <raphael.aleixo.avellar@autodesk.com>
2025-04-06 11:09:47 -0400

    Update url request

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,23 +1,21 @@
 import Link from "next/link";
 import styles from "./Card.module.scss";
 
-const Card = ({ isInternalLink, link, title, introduction, tags }) => {
-  const LinKComponent = isInternalLink ? Link : "a";
-  return (
-    <LinKComponent
-      className={styles.card}
-      href={link}
-      target={isInternalLink ? "" : "_blank"}
-    >
-      <h3>{title}</h3>
-      <p>{introduction}</p>
-      <div className={styles.tags}>
-        {tags ? tags.map((tag) => (
-          <span key={tag}>{tag}</span>
-        )) : false}
-      </div>
-    </LinKComponent>
-  );
+const Card = ({ isInternalLink, link, title, introduction, cityState }) => {
+	const LinkComponent = isInternalLink ? Link : "a";
+	return (
+		<LinkComponent
+			className={styles.card}
+			href={link}
+			target={isInternalLink ? "" : "_blank"}
+		>
+			<h3>{title}</h3>
+			<p>{introduction}</p>
+			<div className={styles.tags}>
+				<span>{cityState}</span>
+			</div>
+		</LinkComponent>
+	);
 };
 
 export default Card;
--- components/NGOList/NGOListGroup.js
+++ components/NGOList/NGOListGroup.js
@@ -3,83 +3,85 @@ import styles from "./NGOList.module.scss";
 import Image from "next/image";
 import Card from "../Card/Card";
 
-
 const requestURLs = {
-  openings:
-    "https://v2.api.atados.com.br/organization/list?limit=5&ordering=-published,-published_date&paused=false&published=true&search=Design",
-  ongs: "https://v2.api.atados.com.br/organization/list?limit=5&ordering=-published,-published_date&published=true",
+	openings:
+		"https://v2.api.atados.com.br/project/list?closed=false&in_review=false&ordering=-published,closed,-published_date&paused=false&published=true&search=design&types=1&limit=5",
+	ongs: "https://v2.api.atados.com.br/organization/list?limit=5&ordering=-published,-published_date&published=true",
 };
 
 const listURLs = {
-  openings: "https://atados.com.br/vagas?query=design",
-  ongs: "https://atados.com.br/ongs",
+	openings: "https://atados.com.br/vagas?query=design",
+	ongs: "https://atados.com.br/ongs",
 };
 
 const linkURLs = {
-  openings: "https://atados.com.br/vaga/",
-  ongs: "https://atados.com.br/ong/",
+	openings: "https://atados.com.br/vaga/",
+	ongs: "https://atados.com.br/ong/",
 };
 
 const NGOListGroup = ({ data }) => {
-  const [list, setList] = useState([]);
-  const [error, setError] = useState([]);
+	const [list, setList] = useState([]);
+	const [error, setError] = useState([]);
 
-  useEffect(() => {
-    const fetchList = async () => {
-      const response = await fetch(requestURLs[data.ngoListType]).catch(() => {
-        setError((oldState) => {
-          const newState = [...oldState];
-          newState.push(data.ngoListType);
-          return newState;
-        });
-      });
-      if (response) {
-        const jsonData = await response.json();
-        const jsonList = jsonData.results;
-        setList(jsonList);
-      }
-    };
-    fetchList();
-  }, [data.ngoListType]);
+	useEffect(() => {
+		const fetchList = async () => {
+			const response = await fetch(requestURLs[data.ngoListType]).catch(() => {
+				setError((oldState) => {
+					const newState = [...oldState];
+					newState.push(data.ngoListType);
+					return newState;
+				});
+			});
+			if (response) {
+				const jsonData = await response.json();
+				const jsonList = jsonData.results;
+				setList(jsonList);
+			}
+		};
+		fetchList();
+	}, [data.ngoListType]);
 
-  return (
-    <article className={styles.list}>
-      <h2>{data.ngoListTitle}</h2>
-      <div dangerouslySetInnerHTML={{ __html: data.ngoListIntro }}></div>
-      {error.includes(data.ngoListType) ? (
-        <div className={styles.error}>
-          Houve um erro ao acessar os dados. Por favor, acesse
-          diretamente o <a href="https://atados.com.br" target="_blank">site da Atados</a> para mais informações.
-        </div>
-      ) : (
-        <div>
-          {list?.map((item) => (
-            <Card
-              key={item.id}
-              isInternalLink
-              link={`${linkURLs[data.ngoListType]}${item.slug}`}
-              introduction={item.description}
-              title={item.organization ? item.organization.name : item.name}
-              tags={item.causes?.map((cause) => cause.name)}
-            />
-          ))}
-          <a
-            href={`${listURLs[data.ngoListType]}`}
-            target="_blank"
-            className={styles.atados}
-          >
-            <Image
-              src="logo-atados.svg"
-              width={32}
-              height={32}
-              alt="Logo do projeto Atados"
-            />
-            <p>{data.ngoListLink}</p>
-          </a>
-        </div>
-      )}
-    </article>
-  );
+	return (
+		<article className={styles.list}>
+			<h2>{data.ngoListTitle}</h2>
+			<div dangerouslySetInnerHTML={{ __html: data.ngoListIntro }} />
+			{error.includes(data.ngoListType) ? (
+				<div className={styles.error}>
+					Houve um erro ao acessar os dados. Por favor, acesse diretamente o{" "}
+					<a href="https://atados.com.br" target="_blank" rel="noreferrer">
+						site da Atados
+					</a>{" "}
+					para mais informações.
+				</div>
+			) : (
+				<div>
+					{list?.map((item) => (
+						<Card
+							key={item.id}
+							isInternalLink
+							link={`${linkURLs[data.ngoListType]}${item.slug}`}
+							introduction={item.description}
+							title={item.organization ? item.organization.name : item.name}
+							cityState={item.address?.city_state}
+						/>
+					))}
+					<a
+						href={`${listURLs[data.ngoListType]}`}
+						target="_blank"
+						className={styles.atados}
+					>
+						<Image
+							src="logo-atados.svg"
+							width={32}
+							height={32}
+							alt="Logo do projeto Atados"
+						/>
+						<p>{data.ngoListLink}</p>
+					</a>
+				</div>
+			)}
+		</article>
+	);
 };
 
 export default NGOListGroup;
